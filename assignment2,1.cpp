be66dca43c5a5c373a3c2ee7805807ac43dad71f
Duy Anh <duyyanhh@gmail.com>
2019-04-24 17:02:05 +0700

    assignment 2

---
+++ assignment2,1.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+	int a;
+	printf("nhap 1 so tu ban phim");
+	scanf("%d",&a);
+	if(a==2){
+		printf("hom nay la thu 2");
+	}else if(a==3){
+		printf("hom nay la thu 3");
+	}else if(a==4){
+		printf("hom nay la thu 4");
+	}else if(a==5){
+		printf("hom nay la thu 5");
+	}else if(a==6){
+		printf("hom nay la thu 6");
+	}else if(a==7){
+		printf("hom nay la thu 7");
+	}else if(a==8){
+		printf("hom nay la chu nhat");
+	}else{
+		printf("khong phai ngay trong tuan");
+	}
+	return 0;
+}
